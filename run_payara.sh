4997dbaea1bdd59e588b5b42fc15d06c8c120c8d
Scholz, Richard <7677934+nextstepman@users.noreply.github.com>
2018-05-02 12:43:15 +0200

    have command files by default have environment variables to control deployment have json logging (and plain as well)

--- run_payara.sh
+++ run_payara.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-exec java $JAVA_BASE_OPTIONS $JAVA_MEMORY_OPTIONS -jar /opt/payara/payara-micro.jar $*
+
+DEPLOY_ARG=""
+if [ -e "$DEPLOY" ]
+then
+    DEPLOY_ARG="--deploy $DEPLOY"
+elif [ -d "$DEPLOYDIR" ]
+then
+    DEPLOY_ARG="--deploydir $DEPLOYDIR"
+fi
+
+exec java $JAVA_BASE_OPTIONS $JAVA_MEMORY_OPTIONS $JAVA_OPTIONS -jar /opt/payara/payara-micro.jar --logproperties $LOG_PROPERTIES_FILE $DEPLOY_ARG $EXTRA_ARGS $*
